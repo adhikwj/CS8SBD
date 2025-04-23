1da240d1c299fb508abf42b338679850eedd9d55
adhikwj <adhikaja@gmail.com>
2025-04-23 19:33:15 +0700

    Minor fix in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Hero from './components/hero';
+import Hero from './components/Hero';
 import TrustedBy from "./components/TrustedBy";
 import CoreFeatures from "./components/CoreFeatures";
 import UseCases from "./components/UseCases";
